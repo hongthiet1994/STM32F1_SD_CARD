4ded6e8a8f7fe3541913829bdf6b4463730b1850
hongthiet1994 <hongthiet1994@gmail.com>
2020-08-26 15:49:45 +0700

    add sdio file

--- EWARM/main.c
+++ EWARM/main.c
@@ -1,5 +1,36 @@
 #include "stm32f10x.h"
 #include "control_led.h"
+#include "stm32_eval_sdio_sd.h"
+
+/** @addtogroup STM32F10x_StdPeriph_Examples
+  * @{
+  */
+
+/** @addtogroup SDIO_uSDCard
+  * @{
+  */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef enum {FAILED = 0, PASSED = !FAILED} TestStatus;
+
+/* Private define ------------------------------------------------------------*/
+#define BLOCK_SIZE            512 /* Block Size in Bytes */
+
+#define NUMBER_OF_BLOCKS      32  /* For Multi Blocks operation (Read/Write) */
+#define MULTI_BUFFER_SIZE    (BLOCK_SIZE * NUMBER_OF_BLOCKS)
+
+#define SD_OPERATION_ERASE          0
+#define SD_OPERATION_BLOCK          1
+#define SD_OPERATION_MULTI_BLOCK    2 
+#define SD_OPERATION_END            3
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+uint8_t Buffer_Block_Tx[BLOCK_SIZE], Buffer_Block_Rx[BLOCK_SIZE];
+uint8_t Buffer_MultiBlock_Tx[MULTI_BUFFER_SIZE], Buffer_MultiBlock_Rx[MULTI_BUFFER_SIZE];
+volatile TestStatus EraseStatus = FAILED, TransferStatus1 = FAILED, TransferStatus2 = FAILED;
+SD_Error Status = SD_OK;
+__IO uint32_t SDCardOperation = SD_OPERATION_ERASE;
 
 
 /* Private typedef -----------------------------------------------------------*/
